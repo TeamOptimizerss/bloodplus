20d3c0ad5281e345ccc599543a0c7ae8d5cfe5c1
Mahesh Cheegiti <ramahesh024@gmail.com>
2023-10-05 00:41:00 +0530

    Added Side Nav Bar

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,12 +1,37 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const SideNav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const [selectedFilter, setSelectedFilter] = useState("Group");
+  const filterRef = useRef(null);
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
+    setIsFilterOpen(false);
   };
+
+  const toggleFilter = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
+  const handleFilterClick = (filter) => {
+    setSelectedFilter(filter);
+    toggleFilter();
+  };
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (filterRef.current && !filterRef.current.contains(event.target)) {
+        setIsFilterOpen(false);
+      }
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
 
   return (
     <Fragment>
@@ -32,24 +57,50 @@ const SideNav = () => {
           </li>
         </ul>
         {isOpen && (
-          <div>
+          <div className="side-nav-div">
             <div className="search-section">
-              <input id="search" type="text" placeholder="Search.." />
+              <div className="filter" onClick={toggleFilter} ref={filterRef}>
+                <i class="fa-solid fa-filter"></i>
+                {selectedFilter}
+                {isFilterOpen && (
+                  <ul className="filter-options">
+                    <li onClick={() => handleFilterClick("Group")}>Group</li>
+                    <li onClick={() => handleFilterClick("Location")}>
+                      Location
+                    </li>
+                  </ul>
+                )}
+              </div>
+              <form>
+                <input id="search" type="text" placeholder="Search.." />
+              </form>
               <label htmlFor="search">
                 <i className="fa-solid fa-magnifying-glass"></i>
               </label>
             </div>
             <div className="hide-icons-section">
-              <h3>Hide Icons</h3>
-              <input type="radio" name="blood" id="hideicons" />
-              <label htmlFor="hideicons">Blood</label>
+              <h3>
+                Hide Icons <i class="fa-regular fa-eye-slash"></i>
+              </h3>
+              <input type="radio" name="bloodhosp" id="blood" />
+              <label htmlFor="blood" id="bloodlabel">
+                <i className="fa-solid fa-droplet"></i> Blood
+              </label>
               <br />
-              <input type="radio" name="hospital" id="hideicons" />
-              <label htmlFor="hideicons">Hospital</label>
+              <input type="radio" name="bloodhosp" id="hospital" />
+              <label htmlFor="hospital" id="hospitallabel">
+                <i className="fa-solid fa-hospital"></i> Hospital
+              </label>
               <br />
             </div>
             <div className="feedback-section">
-              <div className="feedback-text"></div>
+              <div className="feedback-text">
+                Your feedback is valuable to us. It helps us understand your
+                needs and preferences better. Whether you have suggestions for
+                improvements, encountered issues, or just want to share your
+                thoughts, please feel free to let us know. We appreciate your
+                input in making our platform even better for you and others.
+              </div>
               <button className="btn green feedback">FeedBack</button>
             </div>
           </div>
